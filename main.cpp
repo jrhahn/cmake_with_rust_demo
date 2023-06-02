887d7686b09f0bbf1c3f6c868cbf87f19f8e0434
Jürgen Hahn <mail.jhahn@gmail.com>
2023-06-02 11:32:44 +0200

    working

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#ifndef CMAKE_MACRO_EXAMPLE
+  #define CMAKE_MACRO_EXAMPLE "No System Name"
+#endif
+
+#include "test_lib.h"
+
+
+auto sum(int a, int b) {
+  return a + b;
+}
+
+int main() {
+  std::cout << "hello world -> " << sum(40, 2) << std::endl;
+  std::cout << CMAKE_MACRO_EXAMPLE << std::endl;
+  export_fun();
+  return 0;
+}
